e8cfc1e6d4d9cb6630abf5e6c190092172fd2b25
Naftali lewenstein <tolimanman@gmail.com>
2019-05-19 10:50:54 +0300

    Adding second metric option + Beautifying the code
    I have added the option to have a graph with two metrics.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
-import Charts from './Components/Charts';
-import './App.css';
+import React, { Component } from "react";
+import Charts from "./Components/Charts";
+import "./App.css";
 
 /*
-*Wrriten :Naftali Lewenstein
-*email:tolimanman@gmail.com
-*Program name:api-to-chart
-*Description: This program is a website that present a 
-*graph of an api by user choice(Drop down menu).
-*The graph shows all point and values of the api.
-*
-*The user could apply two filters: 
-*1.First input field enter X amount of the data points. This will present to the user the X first data points.
-*2.Second input the user enters a number that wiil be the threshold of the graph. 
-*The threhold is the red line presented on the graph. 
-*
-*After apllying a threshold the website will display all the items that are above the threshold.
-*Enjoy:) 
-*/
+ *Wrriten :Naftali Lewenstein
+ *email:tolimanman@gmail.com
+ *Program name:api-to-chart
+ *Description: This program is a website that present a
+ *graph of an api by user choice(Drop down menu).
+ *The graph shows all point and values of the api.
+ *
+ *The user could apply two filters:
+ *1.First input field enter X amount of the data points. This will present to the user the X first data points.
+ *2.Second input the user enters a number that wiil be the threshold of the graph.
+ *The threhold is the red line presented on the graph.
+ *
+ *After apllying a threshold the website will display all the items that are above the threshold.
+ *Enjoy:)
+ */
 
 /*
 This class is the root of the app its incharge of presenting the 
 drop down menu and acording to the selection to call the child components.
 */
 class App extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
 
     /*Api = the api we are using
@@ -36,105 +35,133 @@ class App extends Component {
              5.valuePath = Path for accessing and mapping values from result of api 
     */
     this.state = {
-    api:'population',
-    data:{
-    category:'Population',
-    apiPath:'https://restcountries.eu/rest/v2/all?fields=name;population;',
-    dataPath:'result.data',
-    labelPath:'e=>(e.name)',
-    valuePath:'e=>(e.population)'
-    }
-    }
-  }
- 
-  //Changes the API adress in state
-  apiChange(value){
-    if(this.state.api!==value.target.value){
-      //loadcomponent only after api update
-    this.setState({api: value.target.value},()=>{this.loadComponent();});
-  }
-  }
-  
-//This function updates the state according to the api value(what the user clicks on the drop down menu)
-loadComponent(){
-  let data={};
-  switch(this.state.api) {
-    case 'transportation':
-    data ={
-      category:'Transportation',
-      apiPath:'http://transport.opendata.ch/v1/locations?query=Zurich',
-      dataPath:'result.data.stations',
-      labelPath:'(e=>e.name)',
-      valuePath:'(e=>e.id)',
-    }
-    this.setState({data:data})
-      break;
-    case 'population':
-    data ={
-    category:'Population',
-    apiPath:'https://restcountries.eu/rest/v2/all?fields=name;population;',
-    dataPath:'result.data',
-    labelPath:'e=>e.name',
-    valuePath:'e=>e.population'
+      api: "population",
+      data: {
+        category: "Population",
+        category2: null,
+        apiPath: "https://restcountries.eu/rest/v2/all?fields=name;population;",
+        dataPath: "result.data",
+        labelPath: "e=>e.name",
+        valuePath: "e=>e.population",
+        valuePath2: null
       }
-    this.setState({data:data}) 
-        break;
-    case 'makeup':
-    data ={
-    category:'Make-up prices',
-    apiPath:'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline',
-    dataPath:'result.data',
-    labelPath:'e=>e.product_type',
-    valuePath:'e=>e.price'
-     }
-    this.setState({data:data}) 
-    break;
-   case 'area':
-   data ={
-   category:'Area',
-   apiPath:'https://restcountries.eu/rest/v2/all?fields=name;area;',
-   dataPath:'result.data',
-   labelPath:'e=>e.name',
-   valuePath:'e=>e.area'
-     }
-     this.setState({data:data}) 
-       break;
-    
-    default:
-    console.log("Error");
-    
+    };
   }
-}
-  
+
+  //Changes the API adress in state
+  apiChange(value) {
+    if (this.state.api !== value.target.value) {
+      //loadcomponent only after api update
+      this.setState({ api: value.target.value }, () => {
+        this.loadComponent();
+      });
+    }
+  }
+
+  //This function updates the state according to the api value(what the user clicks on the drop down menu)
+  loadComponent() {
+    let data = {};
+    switch (this.state.api) {
+      case "transportation":
+        data = {
+          category: "Transportation",
+          category2: null,
+          apiPath: "http://transport.opendata.ch/v1/locations?query=Zurich",
+          dataPath: "result.data.stations",
+          labelPath: "e=>e.name",
+          valuePath: "e=>e.id",
+          valuePath2: null
+        };
+        this.setState({ data: data });
+        break;
+      case "population":
+        data = {
+          category: "Population",
+          category2: null,
+          apiPath:
+            "https://restcountries.eu/rest/v2/all?fields=name;population;",
+          dataPath: "result.data",
+          labelPath: "e=>e.name",
+          valuePath: "e=>e.population",
+          valuePath2: null
+        };
+        this.setState({ data: data });
+        break;
+      case "makeup":
+        data = {
+          category: "Make-up prices",
+          category2: null,
+          apiPath:
+            "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline",
+          dataPath: "result.data",
+          labelPath: "e=>e.product_type",
+          valuePath: "e=>e.price",
+          valuePath2: null
+        };
+        this.setState({ data: data });
+        break;
+      case "area":
+        data = {
+          category: "Area",
+          category2: null,
+          apiPath: "https://restcountries.eu/rest/v2/all?fields=name;area;",
+          dataPath: "result.data",
+          labelPath: "e=>e.name",
+          valuePath: "e=>e.area",
+          valuePath2: null
+        };
+        this.setState({ data: data });
+        break;
+      case "population&Area":
+        data = {
+          category: "Population",
+          category2: "Area",
+          apiPath:
+            "https://restcountries.eu/rest/v2/all?fields=name;population;area;",
+          dataPath: "result.data",
+          labelPath: "e=>e.name",
+          valuePath: "e=>e.area",
+          valuePath2: "e=>e.population"
+        };
+        this.setState({ data: data });
+        break;
+      default:
+        console.log("Error");
+    }
+  }
+
   /*Renders 
   1.Drop down button 
   2.The correct Chart according to the choice
   */
- render(){
+  render() {
+    //Rendering a drop-down menu and a chosen component
+    return (
+      <div className="App">
+        <select
+          className="minimal"
+          value={this.state.api}
+          onChange={this.apiChange.bind(this)}
+        >
+          <option value={"population"}>World poulation</option>
+          <option value={"makeup"}>Make-Up prices</option>
+          <option value={"area"}>Country area's </option>
+          <option value={"transportation"}>Zurich transportation</option>
+          <option value={"population&Area"}>Population + Area</option>
+        </select>
 
- 
-  //Rendering a drop-down menu and a chosen component
-  return (
-    <div className="App">
-
-    <select className="minimal" value={this.state.api} onChange={this.apiChange.bind(this)}>
-    <option value={'population'} >World poulation</option>
-    <option value={'makeup'} >Make-Up prices</option>
-    <option value={'area'} >Country area's </option>
-    <option value={'transportation'} >Zurich transportation</option>
-    </select>
-
-    <Charts 
-      category = {this.state.data.category}
-      apiPath  = {this.state.data.apiPath}
-      dataPath = {this.state.data.dataPath}
-      labelPath= {this.state.data.labelPath}
-      valuePath= {this.state.data.valuePath}
-      />
-     
-    </div>
-  )
- }
+        <Charts
+          category={this.state.data.category}
+          category2={this.state.data.category2}
+          apiPath={this.state.data.apiPath}
+          dataPath={this.state.data.dataPath}
+          labelPath={this.state.data.labelPath}
+          valuePath={this.state.data.valuePath}
+          valuePath2={this.state.data.valuePath2}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/Charts.js
+++ src/Components/Charts.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import './Design.css';
-import axios from 'axios';
-import Graph from './Graph';
+import React, { Component } from "react";
+import "./Design.css";
+import axios from "axios";
+import Graph from "./Graph";
 
 /*
 This class is mainley incharge of collecting the data from the API 
 and sending it to the graph for visualization.
 */
 class Population extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        /*
+    /*
           1.Loading and error = flags to solve asynchronic programming 
           2.Data      = holds data returned from api
           3.Points    = number of lables to print on graph
@@ -21,141 +21,167 @@ class Population extends Component {
           7.labelPath = Path for accessing and mapping labels from result of api
           8.valuePath = Path for accessing and mapping values from result of api 
         */
-        this.state = {
-            loading: true,
-            error: "",
-            data: [],
-            points: 0,
-            threshold:0,
-            apiPath:'',
-            dataPath:'',
-            labelPath:'',
-            valuePath:''
-          };
+    this.state = {
+      loading: true,
+      error: "",
+      data: [],
+      points: 0,
+      threshold: 0,
+      apiPath: "",
+      dataPath: "",
+      labelPath: "",
+      valuePath: "",
+      valuePath2: ""
+    };
+  }
 
+  //Before loading the component update state with props
+  componentWillMount() {
+    this.setState({
+      data: [],
+      apiPath: this.props.apiPath,
+      dataPath: this.props.dataPath,
+      labelPath: this.props.labelPath,
+      valuePath: this.props.valuePath,
+      valuePath2: this.props.valuePath2
+    });
+  }
+
+  //If props from parent changed updtae the state
+  componentDidUpdate(prevProps) {
+    if (
+      this.props.apiPath !== prevProps.apiPath ||
+      this.props.dataPath !== prevProps.dataPath ||
+      this.props.labelPath !== prevProps.labelPath ||
+      this.props.valuePath !== prevProps.valuePath ||
+      this.props.valuePath2 !== prevProps.valuePath2
+    ) {
+      this.setState(
+        {
+          data: [],
+          apiPath: this.props.apiPath,
+          dataPath: this.props.dataPath,
+          labelPath: this.props.labelPath,
+          valuePath: this.props.valuePath,
+          valuePath2: this.props.valuePath2,
+          threshold: 0,
+          points: 0
+        },
+        () => {
+          this.loadData();
+        }
+      );
     }
+  }
 
-    //Before loading the component update state with props
-    componentWillMount(){
-      this.setState({
-         data:[],
-        apiPath:this.props.apiPath,
-        dataPath:this.props.dataPath,
-        labelPath:this.props.labelPath,
-        valuePath:this.props.valuePath
-        
+  //Get Data fro API using AXIOS
+  loadData = () => {
+    this.setState({ loading: true });
+    return axios
+      .get(this.state.apiPath)
+      .then(result => {
+        this.setState({
+          data: eval(this.state.dataPath),
+          loading: false,
+          error: false
+        });
       })
+      .catch(error => {
+        console.error("error: ", error);
+        this.setState({
+          error: `${error}`,
+          loading: false
+        });
+      });
+  };
+
+  //When component finished loading call APi
+  componentDidMount() {
+    this.loadData();
+  }
+
+  //Updates from input the amount of points to be shown
+  pointChange(value) {
+    if (value.target.value === "") {
+      this.setState({ points: 0 });
+    } else if (isNaN(value.target.value)) {
+      console.log("Only numbers aloud!");
+    } else {
+      this.setState({ points: value.target.value });
+    }
+  }
+
+  //Updates from input the threshold line
+  thresholdChange(value) {
+    if (value.target.value === "") {
+      this.setState({ threshold: 0 });
+    } else if (isNaN(value.target.value)) {
+      console.log("Only numbers aloud!");
+    } else {
+      this.setState({ threshold: value.target.value });
+    }
+  }
+
+  //Creates and returns an array with the lables of the graph
+  buttomValues = () => {
+    return this.state.data.map(eval(this.state.labelPath));
+  };
+
+  //Creates and returns an array with the values of the graph
+  sideValues = () => {
+    return this.state.data.map(eval(this.state.valuePath));
+  };
+
+  //Creates and returns an array with the values of the second metric
+  sideValues2 = () => {
+    if (this.state.valuePath2 !== null) {
+      return this.state.data.map(eval(this.state.valuePath2));
+    } else {
+      return {};
+    }
+  };
+
+  render() {
+    //Render graph and inputs(Points and threshold) if the data API call was successfull
+    const { loading, error, data } = this.state;
+    if (loading) {
+      return <p>Loading ...</p>;
+    }
+    if (error) {
+      return (
+        <p>
+          There was an error loading the data.{" "}
+          <button onClick={this.loadData}>Try again</button>
+        </p>
+      );
     }
 
-    //If props from parent changed updtae the state
-    componentDidUpdate(prevProps){
-      if (this.props.apiPath !== prevProps.apiPath||
-        this.props.dataPath !== prevProps.dataPath||
-        this.props.labelPath !== prevProps.labelPath||
-        this.props.valuePath !== prevProps.valuePath){
-        this.setState({ 
-          data:[],
-          apiPath:this.props.apiPath,
-          dataPath:this.props.dataPath,
-          labelPath:this.props.labelPath,
-          valuePath:this.props.valuePath
-        },()=>{this.loadData();});
-        
-      }
-    }
-
-    //Get Data fro API using AXIOS 
-    loadData = () => {
-        this.setState({ loading: true });
-        return axios
-          .get(
-              this.state.apiPath
-          )
-          .then(result => {
-            this.setState({
-              data: eval(this.state.dataPath),
-              loading: false,
-              error: false
-            });
-          })
-          .catch(error => {
-            console.error("error: ", error);
-            this.setState({
-              error: `${error}`,
-              loading: false
-            });
-          });
-      };
-
-    //When component finished loading call APi
-     componentDidMount() {
-        this.loadData();
-     }
-
-     //Updates from input the amount of points to be shown 
-     pointChange(value){
-
-      if(value.target.value===''){
-        this.setState({points: 0});
-      }else if(isNaN(value.target.value)){
-        console.log("Only numbers aloud!");
-      }
-      else{
-        this.setState({points: value.target.value});
-      }
-      }
-
-    //Updates from input the threshold line
-      thresholdChange(value){
-        if(value.target.value===''){
-          this.setState({threshold:0});
-        }else if(isNaN(value.target.value)){
-          console.log("Only numbers aloud!");
-        }
-        else{
-          this.setState({threshold: value.target.value});
-        }
-      }
-
-    //Creates and returns an array with the lables of the graph
-     buttomValues=()=>{
-         let vals = this.state.data.map(eval(this.state.labelPath));
-            return vals;
-      }  
-    //Creates and returns an array with the values of the graph
-      sideValues=()=>{
-        let vals = this.state.data.map(eval(this.state.valuePath));
-           return vals;
-     }  
-     
-     render() {
-        //Render graph and inputs(Points and threshold) if the data API call was successfull
-        const { loading, error, data } = this.state;
-        if (loading) {
-          return <p>Loading ...</p>;
-        }
-        if (error) {
-          return (
-            <p>
-              There was an error loading the data.{" "}
-              <button onClick={this.loadData}>Try again</button>
-            </p>
-          );
-        }
-        
-        return (
-          <div className='box'>
-            <Graph names = {data} 
-                   bottom = {this.buttomValues()}
-                   side={this.sideValues()} 
-                   category={this.props.category} 
-                   points={this.state.points} 
-                   threshold={this.state.threshold}/>
-            <input id ='points' onChange={this.pointChange.bind(this)} placeholder='Amount of points'></input><br></br>
-            <input id ='threshold' onChange={this.thresholdChange.bind(this)} placeholder='Threshold'></input>
-          </div>
-        );
-      }
+    return (
+      <div className="box">
+        <Graph
+          names={data}
+          bottom={this.buttomValues()}
+          side={this.sideValues()}
+          side2={this.sideValues2()}
+          category={this.props.category}
+          category2={this.props.category2}
+          points={this.state.points}
+          threshold={this.state.threshold}
+        />
+        <input
+          id="points"
+          onChange={this.pointChange.bind(this)}
+          placeholder="Amount of points"
+        />
+        <br />
+        <input
+          id="threshold"
+          onChange={this.thresholdChange.bind(this)}
+          placeholder="Threshold"
+        />
+      </div>
+    );
+  }
 }
 
-export default Population
+export default Population;
